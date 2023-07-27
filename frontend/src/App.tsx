6cb286fe42119e583b545a0a0ab59f71da95844d
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-07-27 20:10:59 +0530

    Initial Structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="font-poppins">
+      <h1 className="text-3xl font-bold ">Tailwind is working</h1>
+    </div>
+  );
+}
+
+export default App;
