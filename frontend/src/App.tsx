180224c9d694fc3735a222304a7d2eabb4e5abb6
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-07-28 21:08:47 +0530

    Homepage Done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import Homepage from "./Pages/Homepage";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
     <div className="font-poppins">
-      <h1 className="text-3xl font-bold ">Tailwind is working</h1>
+      {/* <h1 className="text-3xl font-bold ">Tailwind is working</h1> */}
+      <Homepage/>
+      {/* <Sidebar /> */}
     </div>
   );
 }
