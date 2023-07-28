ccb016d7f6b83c25d395d37eec3efa9cd4267991
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-07-28 21:24:35 +0530

    Route to interview pages done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,6 @@ import Allroutes from "./Routes/Allroutes";
 function App() {
   return (
     <div className="font-poppins">
-      {/* <h1 className="text-3xl font-bold ">Tailwind is working</h1> */}
-      {/* <Homepage/> */}
-      {/* <Sidebar /> */}
       <Allroutes />
     </div>
   );
